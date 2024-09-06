f8f485fda543915f2fb55e86b8edfec991d6188b
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-09-06 12:16:12 +0530

    Pending

--- linkedList/doublyLinkedList/DoublyLinkedList.java
+++ linkedList/doublyLinkedList/DoublyLinkedList.java
@@ -1,5 +1,41 @@
 package linkedList.doublyLinkedList;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList<T> {
+  private Node headNode, lastNode;
+  private int size = 0;
+  
+
+  private class Node {
+    T data;
+    Node preNode;
+    Node nextNode;
+
+    public Node(T data) {
+      this.data = data;
+      this.preNode = null;
+      this.nextNode = null;
+      size++;
+    }
+  }
+
+
+  public DoublyLinkedList() {
+    headNode = null;
+    lastNode = null;
+  }
+
+  
+  public void insertFirst(T element) {\
+    Node newNode = new Node(element);
+    if (headNode == null) {
+      headNode = newNode;
+      lastNode = newNode;
+    } else {
+      newNode.nextNode = headNode;
+      lastNode = newNode.nextNode;
+      headNode = newNode;
+    }
+  }
+
   
 }
